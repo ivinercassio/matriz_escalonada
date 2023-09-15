0f9dd039e79595d196ae5c6c09da29d6d6ac09b6
Iviner Ćassio <ivinercassio@gmail.com>
2023-09-15 18:44:49 -0300

    acrescimo

--- GausJordanGAAL/src/App.java
+++ GausJordanGAAL/src/App.java
@@ -4,17 +4,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         Scanner scanner = new Scanner(System.in);
-        int dimensao;
-        do {
-            System.out.print("Informe qual a ordem da matriz quadrada: ");
-            dimensao = scanner.nextInt();       
-        } while (dimensao <= 1);
+        int linhas, colunas;
 
-        Matriz matriz = Matriz.getInstance(dimensao, dimensao);
+        do {
+            System.out.print("Informe quantas linhas tem a matriz: ");
+            linhas = scanner.nextInt();   
+        } while (linhas <= 1);
+        do {
+            System.out.print("Informe quantas colunas tem a matriz: "); // generalizando para matrizes nao quadradas
+            colunas = scanner.nextInt(); 
+        } while (colunas <= 1);
+
+        Matriz matriz = Matriz.getInstance(linhas, colunas);
         matriz.receberMatriz(scanner);
 
-        for (int i = 0; i < dimensao; i++) {
+        for (int i = 0; i < linhas; i++) 
             matriz.escalonarMatriz(i);
-        }
     }
 }
--- GausJordanGAAL/src/Matriz.java
+++ GausJordanGAAL/src/Matriz.java
@@ -20,7 +20,7 @@ public class Matriz {
 
     // the class's methods
     public void receberMatriz(Scanner scanner){
-        System.out.println("PREENCHENDO A MATRIZ...");
+        System.out.println("\nPREENCHENDO A MATRIZ...");
         for (int i = 0; i < this.row; i++) {
             for (int j = 0; j < this.column; j++) {
                 System.out.print("Elemento da linha " + (i+1) + " da coluna " + (j+1) + ": ");
@@ -51,7 +51,7 @@ public class Matriz {
         this.imprimirMatriz();
     }
 
-    public int encontrarPivo(int reference){ 
+    public int encontrarPivo(int reference){ // it means the reference column
         int position = -1;
         for (int i = reference; i < this.column; i++) {    
             if(this.matrix[i][reference] == 1){ 
@@ -72,6 +72,16 @@ public class Matriz {
         }
     }
 
+    public void forcarPivo(int row, int column){ // it means the position where the pivot is waited
+        float divisor = this.matrix[row][column];
+        if(divisor != 0)
+            for (int j = column; j < this.column; j++) 
+                this.matrix[row][j] /= divisor;
+        else
+            if((column+1) < this.column) // search the pivot on the next column
+                forcarPivo(row, (column+1));
+    }
+
     public void zerarLinhasAbaixo(int linhaPivo, int column){ // linhaPivo: this function works below it
         if((linhaPivo+1) == this.row || (column+1) == this.column) 
             return; // when 'linhaPivo' or 'column' is at the end of the matrix
@@ -83,11 +93,5 @@ public class Matriz {
             for (int j = 0; j < matrix.length; j++) 
                 this.matrix[linhaPivo+i][j] -= (fator * this.matrix[linhaPivo][j]);                
         }
-    }
-
-    public void forcarPivo(int row, int column){ 
-        float divisor = this.matrix[row][column];
-        for (int j = column; j < this.column; j++) 
-            this.matrix[row][j] /= divisor;
-    }
+    }    
 }
